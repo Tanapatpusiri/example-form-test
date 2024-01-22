0f7c9d6ecfd89a2db53927a5dcfc27ef752f6d11
bom <tanapat.psr@mail.kmutt.ac.th>
2024-01-22 11:26:49 +0700

    build

---
+++ pages/interface.ts
@@ -0,0 +1,12 @@
+export interface Alluser {
+  "Firstname":string
+  "Lastname":string
+  "Email":string
+  "Pdpa":boolean
+  "Gender":string
+  "Hobby":string[]
+  "Status":string|null
+  "Note":string
+}
+
+
