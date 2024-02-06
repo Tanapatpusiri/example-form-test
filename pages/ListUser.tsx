5bcec566dfe4902c448922d9c9a49986e9408f4b
Tanapatpusiri <tanapat.psr@mail.kmutt.ac.th>
2024-02-06 22:14:55 +0700

    Update ListUser.tsx

--- pages/ListUser.tsx
+++ pages/ListUser.tsx
@@ -9,22 +9,26 @@ interface Props {
 }
 
 const ListUser = ({ user }: Props) => {
-  if (!user || (!user.Firstname && !user.Lastname)) {
-    user.Firstname = '-'
-  }
 
-  const defaultUser = {
-    Firstname: '-',
-    Lastname: '-',
-    Email: '-',
-    Pdpa: false,
-    Gender: '-',
-    Hobby: [],
-    Status: '-',
-    Note: '-'
-  };
+  // const defaultUser = {
+  //   Firstname: '-',
+  //   Lastname: '-',
+  //   Email: '-',
+  //   Pdpa: false,
+  //   Gender: '-',
+  //   Hobby: [],
+  //   Status: '-',
+  //   Note: '-'
+  // };
 
-  user = user || defaultUser;
+
+  // if (!user || (!user.Firstname && !user.Lastname)) {
+  //   user.Firstname = '-'
+  // }
+
+
+
+  // user = user || defaultUser;
  
   return <div>
     
